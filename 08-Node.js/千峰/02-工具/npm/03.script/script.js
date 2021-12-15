68dc819f08fc2491308a9bff7cf82732d1926e9b
coderhxl <coderhxl@outlook.com>
2021-12-16 07:29:23 +0800

    Node

--- 08-Node.js/千峰/02-工具/npm/03.script/script.js
+++ 08-Node.js/千峰/02-工具/npm/03.script/script.js
@@ -1,7 +1,6 @@
 /* 
-    process.env.npm_package_json文件的属性名 固定写法
-        在脚本环境才能访问
-        在npm命令下用来读取 package.json 的某个属性名的值 
+    process.env.npm_package_前缀
+        在脚本环境才能通过npm命令访问在 package.json 的某个属性名的值 
 */
 
 console.log(process.env.npm_package_name)
