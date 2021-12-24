fb6b38c3f5f5485e89ee492f383054060bdfcafb
coderhxl <coderhxl@outlook.com>
2021-12-24 18:20:21 +0800

    Vue

---
+++ 09-Vue2/01-LearnVuejs/08-书籍购物车案例/main.js
@@ -0,0 +1,55 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    boots: [{
+      id: 1,
+      names: '《算法导论》',
+      date: '2006-9',
+      price: 85.00,
+      count: 1,
+    }, {
+      id: 2,
+      names: '《UNIX编程艺术》',
+      date: '2006-2',
+      price: 59.00,
+      count: 1,
+    }, {
+      id: 3,
+      names: '《ES6入门》',
+      date: '2006-9',
+      price: 80.50,
+      count: 1,
+    }, {
+      id: 4,
+      names: '《JavaScript权威指南》',
+      date: '2006-9',
+      price: 128.00,
+      count: 1,
+    }],
+  },
+  methods: {
+    increment(index) {
+      this.boots[index].count++;
+    },
+    decrement(index) {
+      this.boots[index].count--;
+    },
+    removeClick(index) {
+      this.boots.splice(index, 1)
+    },
+  },
+  computed: {
+    totalPrice() {
+      let totalPrice = 0
+      for (const v of this.boots) {
+        totalPrice += v.price * v.count
+      }
+      return totalPrice
+    }
+  },
+  filters: {
+    showPrice(price) {
+      return `￥${price.toFixed(2)}`
+    }
+  },
+})
