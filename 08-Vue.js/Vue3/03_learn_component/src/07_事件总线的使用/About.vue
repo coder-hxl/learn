a05d93e85b1a57df321e3c7d16dca48100b92092
coderhxl <coderhxl@outlook.com>
2022-01-11 20:21:13 +0800

    vue

---
+++ 08-Vue.js/Vue3/03_learn_component/src/07_事件总线的使用/About.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<button @click='btnClick'>按钮</button>
+	</div>
+</template>
+
+<script>
+	import emitter from './utlis/eventbus.js'
+
+	export default {
+		methods: {
+			btnClick() {
+				console.log('About按钮被点击');
+				// 通过emitter的emit方法发送自定义事件及参数
+				emitter.emit('fh', {name: 'fh', age: 17})
+				// emitter.emit('why', {name: 'why', age: 18})
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 08-Vue.js/Vue3/03_learn_component/src/10_作用域插槽的使用/ShowNames.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<template v-for="(items, index) in names">
+				<!-- slot里使用自定义属性将数据传递实例 -->
+				<slot :item='items' :index='index'></slot>
+				
+				<slot name="fuhuan"></slot>
+		</template>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			names: {
+				type: Array,
+				defalut: () => []
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
