89871015f75f528890c73bb6616ea30b1e38d068
coderhxl <coderhxl@outlook.com>
2022-01-12 20:37:23 +0800

    vue

---
+++ 08-Vue.js/Vue3/03_learn_component/src/11_动态组件的使用/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+	<div @click='pageClick'>
+		Home组件: {{name}} - {{age}}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'home',
+		props: {
+			name: {
+				type: String,
+				default: ''
+			},
+			age: {
+				type: Number
+			}
+		},
+		emits: ['pageClick'],
+		methods: {
+			pageClick() {
+				this.$emit('pageClick')
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
