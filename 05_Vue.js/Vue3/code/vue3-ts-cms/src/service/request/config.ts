30435d4795efa69c8c910722b7ee499a2996611d
coderhxl <coderhxl@outlook.com>
2022-04-09 19:20:08 +0800

    vue3-ts-cms

--- 05_Vue.js/Vue3/code/vue3-ts-cms/src/service/request/config.ts
+++ 05_Vue.js/Vue3/code/vue3-ts-cms/src/service/request/config.ts
@@ -16,12 +16,12 @@
 let BASE_URL = ''
 const TIME_OUT = 10000
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV == 'development') {
   BASE_URL = '/api'
-} else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'https://code.org/prod'
+} else if (process.env.NODE_ENV == 'production') {
+  BASE_URL = 'http://152.136.185.210:5000'
 } else {
-  BASE_URL = 'https://code.org/test'
+  BASE_URL = 'https://coderhxl.com/test'
 }
 
 export { BASE_URL, TIME_OUT }
--- 05_Vue.js/Vue3/code/vue3-ts-cms/vite.config.ts
+++ 05_Vue.js/Vue3/code/vue3-ts-cms/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+const path = require('path')
 
 // ElementPlus按需导入样式
 import ElementPlus from 'unplugin-element-plus/vite'
@@ -19,7 +19,7 @@ export default defineConfig({
     ]
   },
   server: {
-    port: 3001,
+    port: 3000,
     host: '0.0.0.0',
     proxy: {
       '/api': {
