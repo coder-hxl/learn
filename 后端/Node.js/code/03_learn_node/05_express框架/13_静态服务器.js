63d9e7b8707b39dcf94b0d0c38b4229ee9ca99fd
coderhxl <coderhxl@outlook.com>
2022-06-23 21:23:54 +0800

    express

--- 后端/Node.js/code/03_learn_node/05_express框架/13_静态服务器.js
+++ 后端/Node.js/code/03_learn_node/05_express框架/13_静态服务器.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static('./build'))
+app.use(express.static('./dist'))
 
 app.listen(8000, () => {
   console.log('静态资源服务器启动成功~')
