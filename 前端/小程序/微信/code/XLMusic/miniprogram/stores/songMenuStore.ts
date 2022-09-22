d85230d8648093b067da992c009a133e6d4c7642
coderhxl <coderhxl@outlook.com>
2022-09-22 20:24:26 +0800

    排行榜 歌单 数据的存储

--- 前端/小程序/微信/code/XLMusic/miniprogram/stores/songMenuStore.ts
+++ 前端/小程序/微信/code/XLMusic/miniprogram/stores/songMenuStore.ts
@@ -8,12 +8,16 @@ const songMenuStore = xlStore({
     choicenessSongMenu: []
   },
   actions: {
-    async fetchRecommendSongMenuAction() {
-      const res = await getRecommendSongMenu(30)
-      this.recommendSongMenu = res.result
+    async fetchRecommendSongMenuAction(limit = 6) {
+      const res = await getRecommendSongMenu(limit)
+      const resMap = res.result.map((item: any) => {
+        return { ...item, coverImgUrl: item.picUrl }
+      })
+
+      this.recommendSongMenu = resMap
     },
-    async fetchChoicenessSongMenuAction() {
-      const res = await getChoicenessSongMenu('全部', 30)
+    async fetchChoicenessSongMenuAction(limit = 6) {
+      const res = await getChoicenessSongMenu('全部', limit)
       this.choicenessSongMenu = res.playlists
     }
   }
