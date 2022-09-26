50e6a6ec42e411edeebcf4d87a3f8bfbe4ca9f2f
coderhxl <coderhxl@outlook.com>
2022-09-26 18:18:43 +0800

    歌曲页面

--- 前端/小程序/微信/code/XLMusic/miniprogram/pages/music-player/music-player.ts
+++ 前端/小程序/微信/code/XLMusic/miniprogram/pages/music-player/music-player.ts
@@ -1,6 +1,10 @@
+import { throttle } from 'underscore'
+
 import { getLyric, getSongDetail } from '../../services/music'
+import { parseLyric } from '../../utils/transition'
 
 const app = getApp()
+const audioContext = wx.createInnerAudioContext()
 
 Page({
   data: {
@@ -10,30 +14,82 @@ Page({
 
     id: 0,
     songData: {},
-    lyric: ''
+    lyricInfo: [] as any[],
+    currentLyricText: '',
+    currentLyricIndex: -1,
+
+    duration: 0,
+    currentTime: 0,
+    sliderValue: 0,
+
+    isSlider: false,
+    isPlay: true
   },
 
   onLoad(options: any) {
+    // 1.获取传入的 id
     const id = options.id
     this.data.id = id
 
+    // 2.根据 id 获取歌信息
+    // 2.1. 获取歌曲信息
     this.fetchSongDetail()
+    // 2.2. 获取详细歌词
     this.fetchLyric()
+
+    // 3.播放歌曲
+    audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`
+    audioContext.autoplay = true
+
+    // 4.监听歌曲播放
+    const throttleUpdateprogress = throttle(this.updateProgress, 500, {
+      leading: false,
+      trailing: false
+    })
+    audioContext.onTimeUpdate(() => {
+      if (!this.data.isSlider) {
+        throttleUpdateprogress()
+
+        let index = -1
+        const lyricLength = this.data.lyricInfo.length
+        for (let i = 0; i < lyricLength; i++) {
+          if (this.data.lyricInfo[i].time >= this.data.currentTime) {
+            index = i - 1
+
+            break
+          }
+        }
+
+        if (index === this.data.currentLyricIndex) return
+
+        const currentLyricText = this.data.lyricInfo[index].text
+        this.setData({
+          currentLyricText,
+          currentLyricIndex: index
+        })
+      }
+    })
+
+    audioContext.onWaiting(() => {
+      audioContext.pause()
+    })
+    audioContext.onCanplay(() => {
+      audioContext.play()
+    })
   },
 
   async fetchSongDetail() {
     const res = await getSongDetail(this.data.id)
-    this.setData({ songData: res.songs[0] })
-    console.log(this.data.songData)
+    this.setData({ songData: res.songs[0], duration: res.songs[0].dt })
   },
 
   async fetchLyric() {
     const res = await getLyric(this.data.id)
-    this.setData({ lyric: res.lrc.lyric })
-    // console.log(this.data.lyric)
+    const lyricInfo = parseLyric(res.lrc.lyric)
+    this.setData({ lyricInfo })
   },
 
-  // 事件
+  // 事件监听
   onSwiperChange(event: any) {
     const current = event.detail.current
     this.setData({ currentPage: current })
@@ -42,5 +98,41 @@ Page({
   onTabTap(event: any) {
     const index = event.currentTarget.dataset.index
     this.setData({ currentPage: index })
+  },
+
+  updateProgress() {
+    const currentTime = audioContext.currentTime * 1000
+    const sliderValue = (currentTime / this.data.duration) * 100
+    this.setData({ currentTime, sliderValue })
+  },
+
+  onSliderChange(event: any) {
+    // 1.滑动结束
+    this.data.isSlider = false
+
+    // 2.计算出要播放位置的时间
+    const value = event.detail.value
+    const currentTime = (value * this.data.duration) / 100
+    audioContext.seek(currentTime / 1000)
+  },
+
+  onSliderChangeing(event: any) {
+    // 1.正在滑动
+    this.data.isSlider = true
+
+    // 2.计算划动对应的播放时间
+    const value = event.detail.value
+    const currentTime = (value * this.data.duration) / 100
+    this.setData({ currentTime })
+  },
+
+  onPauseTap() {
+    if (audioContext.paused) {
+      audioContext.play()
+      this.setData({ isPlay: true })
+    } else {
+      audioContext.pause()
+      this.setData({ isPlay: false })
+    }
   }
 })
--- 前端/小程序/微信/code/XLMusic/miniprogram/utils/transition.ts
+++ 前端/小程序/微信/code/XLMusic/miniprogram/utils/transition.ts
@@ -1,7 +1,30 @@
+const timeReg = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/
+
 function capitalize(str: string) {
   const res = str.slice(0, 1).toUpperCase() + str.slice(1, str.length)
 
   return res
 }
 
-export { capitalize }
+function parseLyric(lyric: string) {
+  const lyricInfo = []
+  const lyricLines = lyric.split('\n')
+
+  for (const item of lyricLines) {
+    const result = timeReg.exec(item) as any[]
+
+    if (!result) continue
+
+    const minute = result[1] * 60 * 1000
+    const second = result[2] * 1000
+    const mSecond = result[3].length === 2 ? result[3] * 10 : result[3] * 1
+    const time = minute + second + mSecond
+    const text = item.replace(timeReg, '')
+
+    lyricInfo.push({ time, text })
+  }
+
+  return lyricInfo
+}
+
+export { capitalize, parseLyric }
